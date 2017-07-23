133993d1d217c5f1af9217087c9a30ae3a838eec
Abhishek Rajamohan <abhishekrajamohan@Abhisheks-MacBook-Pro.local>
2017-07-24 01:45:23 +0530

    Firebase init

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var admin = require("firebase-admin");
+var serviceAccount = require("./keys/gameon-81135-firebase-adminsdk-5cl8k-1a14ece6c6.json");
+
 var app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://gameon-81135.firebaseio.com"
+});
 
 app.set('port', (process.env.PORT || 5000));
 
